a9df6c1bdaf43f9d5ca95607514c6835f953f44a
eliassg <eliasgunnarsson98@gmail.com>
2020-09-14 01:46:44 +0200

    a* implementation finished

--- assignment_2/astar.py
+++ assignment_2/astar.py
@@ -12,6 +12,9 @@ class search_Node:
     
     def __eq__(self, other):
         return self.position == other.position
+    
+    def __gt__(self, other):
+        return (self.g + self.h) > (other.g + other.h)
 
 def manhattan_distance(map_obj, position):
     """ Makes an admissible estimate cost goal """
@@ -20,30 +23,35 @@ def manhattan_distance(map_obj, position):
     return abs(position[0] - goal[0]) + abs(position[1] - goal[1])
 
 def cost_function(map_obj,next_pos):
+    """ Determines cost of a cell so that different cell costs are taken
+        account of in the total cost """
+
     return map_obj.get_cell_value(next_pos)
 
 
 
-def attach_and_eval(map_obj,child, parent, cost):
-    """Attaches a child node to a node that is now considered its best parent"""
+def attach_and_eval(map_obj,child, parent, cost_function):
+    """ Attaches a child node to a node that is now considered its best parent """
     
-    child.parent = parent
-    child.g = parent.g + cost
+    child.best_parent = parent
+    child.g = parent.g + cost_function(map_obj, child.position)
     child.h = manhattan_distance(map_obj, child.position)
 
 def propagate_path_improvements(map_obj, parent):
-    """ Propagation of path improvements through childrend and possible
+    """ Propagation of path improvements through children and possible
         many more descendants """
 
     for kid in parent.kid:
         if parent.g + 1 < kid.g:
-            kid.best_parent = parent
-            kid.g = parent.g + cost_function(map_obj, parent)
+            kid.best_Parent = parent
+            kid.g = parent.g + cost_function(map_obj, parent.position)
             kid.f = kid.g + kid.h
-            propagate_path_improvements(map_obj, kid)
+            propagate_path_improvements(map_obj, kid.position)
 
 
-def best_first_search(map_obj, heuristic_func, cost_func, ):
+def best_first_search(map_obj, heuristic_func, cost_func):
+    """ Implementation of best first search based on handed out pseudocode """
+    
     closed = []
     open_ = []
     #Generate initial node
@@ -62,28 +70,27 @@ def best_first_search(map_obj, heuristic_func, cost_func, ):
         if X == goal_node: #returns path when goal node is found
             path = []
             current_node = X
-            while current_node != None:
+            while current_node is not None:
                 path.append(current_node.position)
-                X = current_node.best_Parent
-            return path.reverse()
+                current_node = current_node.best_Parent
+            return path
 
         kids = []
         directions = {
-            "N": (0,1),
-            "S": (0,-1),
-            "W": (-1,0),
-            "E": (1,0)
+            "N": [0,1],
+            "S": [0,-1],
+            "W": [-1,0],
+            "E": [1,0]
             }
 
         for direction in directions: #generating the nodes kids
-            kid_pos = (X[0] + direction[0], X[1] + direction[1])
+            kid_pos = [X.position[0] + directions[direction][0], X.position[1] + directions[direction][1]]
+            if cost_function(map_obj, kid_pos) == -1:
+            #if map_obj.int_map[kid_pos[0]][kid_pos[1]] == -1:
+                continue
         
-        if cost_function(map_obj, kid_pos) == -1:
-        #if map_obj.int_map[kid_pos[0]][kid_pos[1]] == -1:
-            continue
-        
-        new_sn = search_Node(kid_pos, X)
-        kids.append(new_sn)
+            new_sn = search_Node(kid_pos, X)
+            kids.append(new_sn)
 
         for kid in kids:
             for node in open_:
@@ -95,23 +102,42 @@ def best_first_search(map_obj, heuristic_func, cost_func, ):
         
             X.kids.append(kid)
 
+            if kid not in open_ and kid not in closed:
+                attach_and_eval(map_obj, kid, X, cost_function)
+                open_.append(kid)
+                open_.sort()
+            
+            elif X.g + cost_function(map_obj, kid.position) < kid.g:
+                attach_and_eval(map_obj, kid, X, cost_function)
+                if kid in closed:
+                    propagate_path_improvements(map_obj, X)
 
+
+    return False
+
+def draw_path(map_obj, path):
+    for node_pos in path:
+        map_obj.set_cell_value(node_pos, "Q", True)
         
 
 def main():
     map_obj = Map.Map_Obj(task = 4)
     start_position = map_obj.get_start_pos()
     int_map, str_map = map_obj.get_maps()
-    print(cost_function(map_obj, start_position))
     goal_node = search_Node(map_obj.get_goal_pos())
-    x,y = goal_node.position
-   
-   
+    
+    path = best_first_search(map_obj, manhattan_distance, cost_function)
+    print("Starting position: ", start_position)
+    print("Goal position: ", goal_node.position)
+
+
+    try:
+        draw_path(map_obj, path)
+        print("Drawing path...")
+    except TypeError:
+        print("Path does not exist")
+
     map_obj.show_map()
-
-    print(manhattan_distance(map_obj, start_position))
-    print(map_obj.int_map[start_position[0]][start_position[1]])
-
 
 
 if __name__ == "__main__":
